a4973df1b35f9054ae9d0993a760afcc3b0a199b
Terross <dima38091@gmail.com>
2020-11-22 15:26:28 +0300

    All tabs and exceptions

--- springboot-backend/src/main/java/com/example/demo/SpringbootBackendApplication.java
+++ springboot-backend/src/main/java/com/example/demo/SpringbootBackendApplication.java
@@ -15,16 +15,5 @@ public class SpringbootBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
 	}
-//	@Bean
-//    CommandLineRunner runner(ProductRepository productRepository) {
-//        return args -> {
-//            productRepository.save(new Product(1L, "TV Set", 300.00));
-//            productRepository.save(new Product(2L, "Game Console", 200.00));
-//            productRepository.save(new Product(3L, "Sofa", 100.00));
-//            productRepository.save(new Product(4L, "Icecream", 5.00));
-//            productRepository.save(new Product(5L, "Beer", 3.00));
-//            productRepository.save(new Product(6L, "Phone", 500.00));
-//            productRepository.save(new Product(7L, "Watch", 30.00));
-//        };
-//    }
+
 }
